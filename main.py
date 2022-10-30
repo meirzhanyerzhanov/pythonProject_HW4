61c019d6ab4efc70f8c05fcd04390b0851edb362
Meirzhan <meirzhan.yerzhanov@alumni.nu.edu.kz>
2022-10-30 22:49:12 +0900

    1

--- main.py
+++ main.py
@@ -1,44 +1,61 @@
-from typing import List
-import json
+import keyword
 
 
-class Advert:
+class My_dict:
+    """
+    sets attributes and checks for reserved words
+    """
 
-    def __init__(self, my_dict):
-        self.one_more_dict = my_dict.copy()
-        if 'price' not in my_dict:
+    def __init__(self, my_dict: dict):
+        for key, value in my_dict.items():
+            key_check = key
+            if keyword.iskeyword(key):
+                key_check = key + '_'
+
+            if isinstance(value, dict):
+                value = My_dict(value)
+            setattr(self, key_check, value)
+
+    def __str__(self):
+        return f'{self.title} | {self.price} ₽'
+
+
+class ColorizeMixin():
+    """
+    Changes color of output
+    """
+
+    def __str__(self):
+        title = super().__dict__['title']
+        price = super().__dict__['price']
+
+        return f'\033[1;{self.repr_color_code};10m {title} | {price} ₽'
+
+
+class Advert(ColorizeMixin, My_dict):
+    """
+    Using parent class (My_dict), imposes conditions on the attributes
+    """
+    repr_color_code = 33
+
+    def __init__(self, my_dict: dict):
+        super().__init__(my_dict)
+        if 'price' not in self.__dict__:
             my_dict['price'] = 0
         elif my_dict['price'] < 0:
             raise KeyError('must be >=0')
-        self.text_dict = my_dict
-
-    def __repr__(self):
-        return f'{self.title} | {self.price} ₽'
-
-    def return_output(self, item):
-        if isinstance(item, dict):
-            return Advert(item)
-        else:
-            return item
-
-    def __getattr__(self, item):
-        if item in self.text_dict:
-            return self.return_output(self.text_dict[item])
-        else:
-            raise AttributeError("No category")
-
-    def __str__(self):
-        return f'{self.one_more_dict}'
+        if 'title' not in self.__dict__:
+            raise ValueError('must contain title')
 
 
 if __name__ == '__main__':
     a = {
-"title": "Вельш-корги",
-"price": 1000,
-"class": "dogs",
-"location": {
-"address": "сельское поселение Ельдигинское, поселок санатория Тишково, 25"
-}
-}
+        "title": "Вельш-корги",
+        "price": 100,
+        "class": "dogs",
+        "location": {
+            "address": "сельское поселение Ельдигинское, 25"
+        }
+    }
     lesson_ad = Advert(a)
-    print(lesson_ad.class_)
+    print(lesson_ad)
